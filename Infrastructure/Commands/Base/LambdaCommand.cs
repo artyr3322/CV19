0bfe8e5d95801628992f59933e7c7a1f2372df17
Artyr <Artyr@DESKTOP-BH5V7J7>
2021-01-20 14:56:06 +0200

    Главный класс команды для нашего приложения

---
+++ Infrastructure/Commands/Base/LambdaCommand.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV19.Infrastructure.Commands.Base
+{
+    internal class LambdaCommand : Command
+    {
+        //Это класс команды который непосредственно у нас будет выполняться, его обьекты станут командами в модели-представления
+
+        //Поля с аттрибутом только для чтения(readonly) работают быстрее стандартных полей
+
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+
+
+        //Конструктор класса команды
+        //В конструкторе надо получить 2 делегата: первфй будет выполняться методом CanExecute(), второй будет выполняться методом Execute
+
+
+        public LambdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            //?? означает проверку на null. Если первая переменная = null - подставляется вторая. Пример: a = b ?? с
+            //Тоесть поле _Execute принимает либо значение введенного параметра Execute либо выдает исключение
+            //Функция nameof() возвращает имя элемента в строчном формате
+
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExecute = CanExecute;
+        }
+
+        //Возвращаем либо true(дает добро на выполнение метода Execute(тоесть самой команды), 
+        //либо если поле _CanExecute не равно Null возвращаем результат выполнения делегата _CanExecute
+        //поле _CanExecute преедставляет из себя некое условие, которое определяет будет ли команда выполняться
+
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        //Реализация метода без лямбды
+
+        //public override bool CanExecute(object parameter)
+        //{
+        //   return _CanExecute?.Invoke(parameter) ?? true;
+        //}
+
+
+        //Так как на Null параметр Execute мы проверили еще в конструкторе просто выполним метод-деелегат _Execute(parameter
+        public override void Execute(object parameter) => _Execute(parameter);
+
+        //Реализация без лямбды
+       // public override void Execute(object parameter)
+       // {
+       //     _Execute(parameter);
+       // }
+    }
+}
--- ViewModels/Base/MainViewModel.cs
+++ ViewModels/Base/MainViewModel.cs
@@ -27,24 +27,24 @@ namespace CV19.ViewModels.Base
             //можно записать блок get так
             //get { return _Title}
 
-            get => _Title;
-            set => Set(ref _Title, value);
-            //{
-
             //Используем метод Equals(): Если новое значение свойства совпадает со значением поля yичего не выполнять
             //Это нуно чтобы не нагружать систему ненужными действиями
-            // if (Equals(_Title, value)) return;
+            // {if (Equals(_Title, value)) return;
             //_Title = value;
-            //OnPropertyChanged();
+            //OnPropertyChanged();}
 
             //Так как у нас в базовом классе есть метод Set
             //мы можем записать данный блок так:
-            //Set(ref _Title, value);
+            //{Set(ref _Title, value);}
 
             //Или как вверху, еще проще, все операции выполнит метод Set: сравнит значение поля с новым значением свойства, если они не совпадают перезапишет поле
             //и применит метод OnPropertyChanged
-            //}
 
+
+
+            get => _Title;
+            set => Set(ref _Title, value);
+            
 
         }
 
